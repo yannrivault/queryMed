7d0487eb69023a6dd6f8badfeb599e05591507e6
NOLWENN LE MEUR <nlemeur@gmail.com>
2018-04-04 08:57:57 +0200

    Adding vignette

---
+++ queryMed/vignettes/Howto-queryMed.R
@@ -0,0 +1,39 @@
+---
+  title: "queryMed package"
+author: "Y. Rivault, O.Dameron and N. Le Meur"
+date: "03/04/2018"
+output: pdf_document
+---
+  
+  ```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = TRUE)
+```
+
+## Context
+
+In the era massive open-data access and big data in general, pharmaco-epidemiology and public health sciences are in need for bioinformatics tools. Researchers can now query large medical information systems such as medico-administrative databases and claim databases. They thus need batch translation of drug and disease codes to filter out known and trivial associations but also identify critical interactions for patient safety. 
+
+Standard medical terminologies/ontologies facilitates 
+
+We propose a tool to integrate ontologies programmatically in the R environment.
+
+
+
+```{r cars, eval=F}
+library(queryMed)
+```
+
+## Material and Methods
+
+BIOPORTAL
+
+SPARQL endpoints
+
+List of accessible ontologies
+
+## Applications
+
+
+
+## References
+Natalja Kurbatova, Tomasz Adamusiak, Pavel Kurnosov, Morris A. Swertz, Misha Kapushesky; ontoCAT: an R package for ontology traversal and search, Bioinformatics, Volume 27, Issue 17, 1 September 2011, Pages 2468–2470, https://doi.org/10.1093/bioinformatics/btr375
