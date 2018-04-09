f0c80e8dff8b1e16cc75910580d1f2931c4aba3b
Utilisateur local EHESP <ehesp@INF03789.ecole.root.priv>
2018-04-09 13:22:41 +0200

    updated NDFRT CI querry

--- queryMed/R/NDFRT_CI_with.R
+++ queryMed/R/NDFRT_CI_with.R
@@ -1,4 +1,4 @@
-NDFRT_CI_with <- function(atc=NULL,api_key=api_key){
+NDFRT_CI_with <- function(api_key=""){
   
   query="
   prefix owl: <http://www.w3.org/2002/07/owl#>
@@ -6,7 +6,7 @@ NDFRT_CI_with <- function(atc=NULL,api_key=api_key){
   prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
   prefix umls: <http://bioportal.bioontology.org/ontologies/umls/>
   
-  SELECT DISTINCT ?cui_drug ?cui_diag
+  SELECT DISTINCT ?ndf_drug ?ndf_diag ?cui_drug ?cui_diag
   FROM <http://evs.nci.nih.gov/ftp1/NDF-RT/NDF-RT.owl>
   WHERE {
   
@@ -22,9 +22,9 @@ NDFRT_CI_with <- function(atc=NULL,api_key=api_key){
   
   results=sparql(url="http://sparql.hegroup.org/sparql/",query=query)
   
-  cui_atc=mapping_atc_cui(codes=results$cui_drug,source="cui",api_key=api_key)
-  cui_icd10=mapping_icd10_cui(api_key=api_key)
-
+  cui_atc=mapping_cui_search(codes=results$cui_drug,ontology="ATC",source="cui",api_key=api_key)
+  cui_icd10=mapping_cui_search(codes=results$cui_diag,ontology="ICD10",source="cui",api_key=api_key)
+  
   results=merge(results,unique(merge(results,cui_atc,by.x="cui_drug",by.y="cui")),all.x=T)
   results=merge(results,unique(merge(results,cui_icd10,by.x="cui_diag",by.y="cui")),all.x=T)
   
