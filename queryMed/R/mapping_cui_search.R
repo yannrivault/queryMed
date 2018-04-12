c373f857116392938036045c037415dfad4c1369
Utilisateur local EHESP <ehesp@INF03789.ecole.root.priv>
2018-04-12 16:37:35 +0200

    added general cui mapping function

---
+++ queryMed/R/mapping_cui_search.R
@@ -0,0 +1,34 @@
+mapping_cui_search <- function(codes=NULL,ontology="ATC",source="cui",api_key=""){
+  
+  if (source=="cui"){
+    codes=unlist(str_extract_all(unique(codes),"C[0-9]{7}"))
+  }
+  
+  n <- length(codes) %/% 800
+  rest <- length(codes) %% 800
+  
+  results <- data.frame()
+  
+  if (n>0){
+    for(i in 1:n){
+      S=search_endpoint(term = paste(codes[((i-1)*800):(i*800-1)],collapse="+"), ontologies = ontology, service = "bioportal", api_key = api_key, extra_args = "&display_context=false&display_links=false")
+      if (!is.null(S)){
+        for(j in 1:length(S$collection)){
+          results[dim(results)[1]+1,ontology]=S$collection[[j]]$"@id"
+          results[dim(results)[1],"cui"]=S$collection[[j]]$cui
+        }
+      }
+    }
+  }
+  if (rest>0){
+    S=search_endpoint(term = paste(codes[(n*800):(n*800+rest)],collapse="+"), ontologies = ontology, service = "bioportal", api_key = api_key, extra_args = "&display_context=false&display_links=false")
+    if (!is.null(S)){
+      for(j in 1:length(S$collection)){
+        results[dim(results)[1]+1,ontology]=S$collection[[j]]$"@id"
+        results[dim(results)[1],"cui"]=S$collection[[j]]$cui
+      }
+    }
+  }
+  
+  return(results)
+}
