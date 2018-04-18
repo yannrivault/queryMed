df4e4c52d2c9a465365886ad5899b61bd8f5a2e7
Utilisateur local EHESP <ehesp@INF03789.ecole.root.priv>
2018-04-18 14:16:16 +0200

    improved mapping to cui function (mapping can go from cui to an ontology and vice versa added also a progress bar option)

--- queryMed/R/mapping_cui_search.R
+++ queryMed/R/mapping_cui_search.R
@@ -1,11 +1,11 @@
-mapping_cui_search <- function(codes=NULL,ontology="",source="cui",api_key=""){
+mapping_cui <- function(codes=NULL,ontology="",api_key="",progress=TRUE){
   
-  if (source=="cui"){
-    codes=unlist(str_extract_all(unique(codes),"C[0-9]{7}"))
-  }
+  codes <- unique(codes)
   
   n <- length(codes) %/% 800
   rest <- length(codes) %% 800
+  
+  if(progress==TRUE) progress_bar <- txtProgressBar(min = 0, max = length(codes), style = 3)
   
   results <- data.frame()
   
@@ -14,29 +14,31 @@ mapping_cui_search <- function(codes=NULL,ontology="",source="cui",api_key=""){
       S=search_endpoint(term = paste(codes[((i-1)*800):(i*800-1)],collapse="+"), ontologies = ontology, service = "bioportal", api_key = api_key, extra_args = "&display_context=false&display_links=false")
       if (!is.null(S)){
         for(j in 1:length(S$collection)){
-          for(k in 1:length(S$collection[[j]]$cui)){
-            if(S$collection[[j]]$cui[[k]] %in% codes){
+          if((uri2norm(S$collection[[j]]$'@id') %in% codes) | (length(intersect(unlist(S$collection[[j]]$cui),codes))>0)){
+            for(k in 1:length(S$collection[[j]]$cui)){
               results[dim(results)[1]+1,"cui"]=S$collection[[j]]$cui[[k]]
-              results[dim(results)[1],"mapping"]=S$collection[[j]]$"@id"
+              results[dim(results)[1],"classe"]=S$collection[[j]]$"@id"
             }
           }
         }
       }
+      if(progress==TRUE) setTxtProgressBar(progress_bar, i*800)
     }
   }
   if (rest>0){
     S=search_endpoint(term=paste(codes[(n*800):(n*800+rest)],collapse="+"), ontologies = ontology, service = "bioportal", api_key = api_key, extra_args = "&display_context=false&display_links=false")
     if (!is.null(S)){
       for(j in 1:length(S$collection)){
-        for(k in 1:length(S$collection[[j]]$cui)){
-          if(S$collection[[j]]$cui[[k]] %in% codes){
+        if((uri2norm(S$collection[[j]]$'@id') %in% codes) | (length(intersect(unlist(S$collection[[j]]$cui),codes))>0)){
+          for(k in 1:length(S$collection[[j]]$cui)){
             results[dim(results)[1]+1,"cui"]=S$collection[[j]]$cui[[k]]
-            results[dim(results)[1],"mapping"]=S$collection[[j]]$"@id"
+            results[dim(results)[1],"classe"]=S$collection[[j]]$"@id"
           }
         }
+        if(progress==TRUE) setTxtProgressBar(progress_bar, n*800+rest)
       }
     }
   }
-  
-  return(results)
+  if(progress==TRUE) close(progress_bar)
+  return(uri2norm(results))
 }
