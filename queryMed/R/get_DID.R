64b2bea81c846a19b432548dcd4920956d29d4b7
NOLWENN LE MEUR <nlemeur@gmail.com>
2018-05-16 10:45:54 +0200

    Complete Help page and clean get_DID and get_DIKB functions.

--- queryMed/R/get_DID.R
+++ queryMed/R/get_DID.R
@@ -1,6 +1,12 @@
 # I don't know yet if it is preferable to load the database in the package, instead of having a function that load it.
 
-get_DID <- function(mapping_atc=TRUE,mapping_icd10=TRUE,api_key=""){
+get_DID <- function(mapping_atc=FALSE,mapping_icd10=FALSE,api_key=""){
+  
+  if(mapping_icd10==TRUE & api_key=="" | mapping_atc==TRUE & api_key==""){
+    warning("A valid api_key value is required to perform the mapping")
+    return(NULL)
+  }  
+  
   url="https://static-content.springer.com/esm/art%3A10.1186%2Fs13326-016-0110-0/MediaObjects/13326_2016_110_MOESM1_ESM.xlsx"
   DID <- read.xlsx(url, sheet=3, colNames=F)
   colnames(DID) = gsub("[[:space:]]","_",gsub("NA.","",paste(c("NA","NA","NA",DID[1,4:length(DID)]),DID[2,],DID[3,],sep=".")))
