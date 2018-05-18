3693d75adfedd79f1fa9a9987ea34e0b9f375c0c
Utilisateur local EHESP <ehesp@INF03789.ecole.root.priv>
2018-05-18 16:16:09 +0200

    gsub instead of only sub

--- queryMed/R/uri2norm.R
+++ queryMed/R/uri2norm.R
@@ -2,8 +2,7 @@ uri2norm <-
 function(X){
   X <- as.matrix(X)
   class(X) <- "character"
-  X <- sub(".*/", "", X)
-  X <- sub(">", "", X)
-  X <- sub("(^\\s*|\\s*$)","",X, perl=T)
+  X <- gsub(".*/|>", "", X)
+  X <- gsub("(^\\s*|\\s*$)","",X, perl=T)
   return(as.data.frame(X))
 }
