3c80fa83235c24eba03c383883a8408f9d597ed7
Utilisateur local EHESP <ehesp@INF03789.ecole.root.priv>
2018-05-18 15:50:12 +0200

    small update

--- queryMed/R/uri2norm.R
+++ queryMed/R/uri2norm.R
@@ -1,16 +1,17 @@
 uri2norm <- 
 function(X){
   X <- as.matrix(X)
+  class(X) <- "character"
   for(i in 1:length(X)){
     if (X[i] %in% c(NA,"")){}
     else {
-      A <- as.character(X[i])
-      A <- strsplit(A,"/")
+      #A <- as.character(X[i])
+      A <- strsplit(X[i],"/")
       A <- gsub("^.*#","",A[[1]][length(A[[1]])])
       A <- gsub("^.*:","",A)
       A <- gsub(">","",A)
       X[i] <- gsub("(^\\s*|\\s*$)","",A, perl=T)
     }
   }
-  return(X)
+  return(as.data.frame(X))
 }
