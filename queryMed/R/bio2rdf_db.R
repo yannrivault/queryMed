b8282eff7cbfdcdbc2f1e4dea8b8a6ff362bbd21
Utilisateur local EHESP <ehesp@INF03789.ecole.root.priv>
2018-05-25 12:01:16 +0200

    high level function for querying bio2rdf about db drugs

---
+++ queryMed/R/bio2rdf_db.R
@@ -0,0 +1,20 @@
+# bio2rdf
+
+bio2rdf_db <- function(lang="en"){
+  
+  lang=paste(lang,collapse="\",\"")
+  
+  query=paste(c(
+    "SELECT DISTINCT *
+  WHERE  {
+    ?db <http://bio2rdf.org/drugbank_vocabulary:x-atc> ?atc .
+    OPTIONAL {?db rdfs:label ?label .
+    FILTER(lang(?label) IN (\"","\"))}
+    OPTIONAL {?db dcterms:description ?description .
+    FILTER(lang(?description) IN (\"","\"))}
+    OPTIONAL {?db <http://bio2rdf.org/drugbank_vocabulary:category> ?category .}
+    
+  }"),collapse=lang)
+  
+  return(sparql(query,url="http://bio2rdf.org/sparql"))
+}
