5e0de8685c2064d74a61d11200a33fee3cda1e79
yannrivault <yann.rivault@ehesp.fr>
2018-06-27 13:58:56 +0200

    updated for working with two databases

--- queryMed/R/find_relations.R
+++ queryMed/R/find_relations.R
@@ -1,13 +1,30 @@
-find_relations <- function(data,data_indices,data_elements,target,target_elements){
+find_relations <- function(data.x,data_indices,data.y=NULL,data_elements.x=NULL,data_elements.y=NULL,target,target_elements){
   
-  data_vocab = as.vector(unlist(t(data[,data_elements])))
-  target_vocab = as.vector(unlist(t(target[,target_elements])))
-  vocab = intersect(data_vocab,target_vocab)
+  if(!is.null(data.y)){
+    data_vocab = unique(c(as.vector(unlist(t(data.x[,data_elements.x]))),as.vector(unlist(t(data.y[,data_elements.y])))))
+    target_vocab = unique(as.vector(unlist(t(target[,target_elements]))))
+    vocab = intersect(data_vocab,target_vocab)
+    
+    data.x <- data.x[,c(data_indices,data_elements.x)]
+    data.y <- data.y[,c(data_indices,data_elements.y)]
+    
+    data.x <- gather(data.x,key=data_element_attribute,value=elements,data_elements.x)
+    data.y <- gather(data.y,key=data_element_attribute,value=elements,data_elements.y)
+    
+    data <- rbind(data.x,data.y)
+    data <- data[(data[,"elements"]%in%vocab),]
+  }
   
-  data <- data[,c(data_indices,data_elements)]
-  
-  data <- gather(data,key=data_element_attribute,value=elements,data_elements)
-  data <- data[(data[,"elements"]%in%vocab),]
+  else{
+    data_vocab = as.vector(unlist(t(data.x[,data_elements.x])))
+    target_vocab = as.vector(unlist(t(target[,target_elements])))
+    vocab = intersect(data_vocab,target_vocab)
+    
+    data <- data[,c(data_indices,data_elements.x)]
+    
+    data <- gather(data,key=data_element_attribute,value=elements,data_elements.x)
+    data <- data[(data[,"elements"]%in%vocab),]
+  }
   
   target <- target[as.character(target[,target_elements[1]])%in%vocab & as.character(target[,target_elements[2]])%in%vocab, ]
   
