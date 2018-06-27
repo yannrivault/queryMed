4566d96635751562ee825b3f8889bc68afe5b4b7
yannrivault <yann.rivault@ehesp.fr>
2018-06-27 14:23:40 +0200

    debugged

--- queryMed/R/uri2norm.R
+++ queryMed/R/uri2norm.R
@@ -5,8 +5,8 @@ uri2norm <- function(X){
     class(X) <- "character"
     uris <- apply(X,FUN=function(x) str_detect(x,"http"), MARGIN=2)
 
-    X[uris] <- gsub(".*/|>", "", X[uris])
-    X[uris] <- gsub("(^\\s*|\\s*$)","",X[uris], perl=T)
+    X[which(uris)] <- gsub(".*/|>", "", X[which(uris)])
+    X[which(uris)] <- gsub("(^\\s*|\\s*$)","",X[which(uris)], perl=T)
     return(as.data.frame(X))
     }
   else {
