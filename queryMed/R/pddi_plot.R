39e1a7707ba0298efcb126b4852ebca5f201864b
NOLWENN LE MEUR <nlemeur@gmail.com>
2018-07-05 19:43:05 +0200

    Man page DIKB adding do not run example Fix pddi_plot function call

--- queryMed/R/pddi_plot.R
+++ queryMed/R/pddi_plot.R
@@ -1,8 +1,8 @@
 ##################@
 pddi_plot <- function(drug= "", type="name", direction="object", source=NULL, contraindication= NULL, plot=TRUE, level=4, mypalette=NULL, weight=NULL){
-  
-  DIKB<- utils::data(DIKB)
-  ATC <- utils::data(ATC)  
+
+  utils::data(DIKB)
+  utils::data(ATC)  
   lev <- paste("atc",level, sep="")
   if(level >1){
   levm1 <- paste("atc",level-1, sep="")
@@ -11,7 +11,7 @@ pddi_plot <- function(drug= "", type="name", direction="object", source=NULL, co
     levm1 <- lev
   }
   lab <- paste("label",level, sep="")
-  
+
   # filter by data sources     
   if(!is.null(source)){
     DIKB <-DIKB[DIKB$source%in%source,]
@@ -19,7 +19,7 @@ pddi_plot <- function(drug= "", type="name", direction="object", source=NULL, co
       return(warning("No data for the source"))
     }
   }
-  
+
   # looking more specifically for contraindication (or not)
   if(!is.null(contraindication)){
     if(contraindication==TRUE){
@@ -47,7 +47,8 @@ pddi_plot <- function(drug= "", type="name", direction="object", source=NULL, co
     if(is.null(nrow(x))) {
       return(warning("No data found"))
     }
-  }  
+  }
+
   if(direction=="precipitant"){
       x <- switch(type,
                   name = DIKB[DIKB$precipitant==drug,],
@@ -59,25 +60,25 @@ pddi_plot <- function(drug= "", type="name", direction="object", source=NULL, co
   }    
   x = as.data.table(x)
   x <- x[!is.na(x$atc2)|!is.na(x$atc1),]
-   
+
   if(direction=="object"){
-    statx <- x[, .N, by=list("precipitant", "atc2", "drug2")]
+    statx <- x[, .N, by=list(precipitant, atc2, drug2)]
   }
   if(direction=="precipitant"){
-    statx <- x[, .N, by=list("object", "atc1", "drug1")]
+    statx <- x[, .N, by=list(object, atc1, drug1)]
   }
   statx <- data.frame(statx)
   names(statx) <-   c("interact", "atc5", "dbi", "n")
-  statx <- merge(statx, ATC[, c("atc5", "lev", "lab", "levm1")], by="atc5")
+  statx <- merge(statx, ATC[, c("atc5", lev, lab, levm1)], by="atc5")
   
   if(plot==TRUE){
 
     statx <- as.data.table(statx)
-    statx<-statx[,.(n=sum(n)), by=c("lab", "levm1")]
+    statx<-statx[,.(n=sum(n)), by=c(lab, levm1)]
     statx <- data.frame(statx)
 
    
-    root <- unique(statx[, "levm1"])
+    root <- unique(statx[, levm1])
     n = length(root)
       #mypalette<- colorRampPalette(brewer.pal(12,"Set3"))(n)
       #mypalette<- colorRampPalette(brewer.pal(9,"YlOrRd"))(n)
@@ -96,6 +97,7 @@ pddi_plot <- function(drug= "", type="name", direction="object", source=NULL, co
 
     g1 <- graph_from_edgelist(as.matrix(cbind(drug, as.character(statx[,2]))), directed=F)
     statx$n[statx$n == 1 ] <- 0
+    #g1 <- set_edge_attr(g1, weight, E(g1),  statx$n)
     E(g1)$weight <- statx$n
     E(g1)$width <- 1+E(g1)$weight*2
     V(g1)$size <- 20
