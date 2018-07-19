616b89e22f18bce996c8fec4f92dc2b465ce8678
yannrivault <yann.rivault@ehesp.fr>
2018-07-19 13:20:27 +0200

    another test...

--- queryMed/R/test.R
+++ queryMed/R/test.R
@@ -1,3 +1,4 @@
+require(rrdf)
 test <- function(path="",apikey=""){
   download.file(paste("http://data.bioontology.org/ontologies/ATC/submissions/9/download?apikey=",apikey,sep=""),path)
   exData = load.rdf(path, format="TURTLE")
