f369a1c52e91f4fceb726ec368178045b7aeb29e
yannrivault <yann.rivault@ehesp.fr>
2018-07-19 14:47:23 +0200

    update

--- queryMed/R/get_ontology_from_bioportal.R
+++ queryMed/R/get_ontology_from_bioportal.R
@@ -1,7 +1,6 @@
-require(rrdf)
 get_ontology_from_bioportal <- function(ontology, path="", apikey=""){
   download.file(paste(c("http://data.bioontology.org/ontologies/",toupper(ontology),"/download?apikey=",apikey),collapse=""),paste(path,"ATC",""))
-  exData = load.rdf(paste(path,"ATC",""), format="TURTLE")
-  print(class(exData))
+  ontology = load.rdf(paste(path,"ATC",""), format="TURTLE")
+  return(ontology)
   
 }
