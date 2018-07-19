74c97940234f091a2831e4012a727896cd77e3ab
yannrivault <yann.rivault@ehesp.fr>
2018-07-19 13:05:45 +0200

    small update for tests

--- queryMed/R/test.R
+++ queryMed/R/test.R
@@ -1,6 +1,6 @@
-test <- function(){
-  download.file("http://data.bioontology.org/ontologies/ATC/submissions/9/download?apikey=8b5b7825-538d-40e0-9e9e-5ab9274a9aeb","/home/ehesp/Téléchargements/ATC.ttl")
-  exData = load.rdf("/home/ehesp/Téléchargements/ATC.ttl", format="TURTLE")
+test <- function(path=""){
+  download.file("http://data.bioontology.org/ontologies/ATC/submissions/9/",path)
+  exData = load.rdf(path, format="TURTLE")
   print(exData)
   
 }
