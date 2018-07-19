a8326c4812ff3992c96df66f3ad41554d135a91e
yannrivault <yann.rivault@ehesp.fr>
2018-07-19 14:58:17 +0200

    removed path option, downloads are into the package lib

--- queryMed/R/get_ontology_from_bioportal.R
+++ queryMed/R/get_ontology_from_bioportal.R
@@ -1,4 +1,8 @@
-get_ontology_from_bioportal <- function(ontology, path="", apikey=""){
+get_ontology_from_bioportal <- function(ontology, apikey=""){
+  
+  path=file.path(file.path(find.package("queryMed"), c("data"),"ontologies"))
+  dir.create(path, showWarnings = FALSE)
+  
   download.file(paste(c("http://data.bioontology.org/ontologies/",toupper(ontology),"/download?apikey=",apikey),collapse=""),paste(path,"ATC",""))
   ontology = load.rdf(paste(path,"ATC",""), format="TURTLE")
   return(ontology)
