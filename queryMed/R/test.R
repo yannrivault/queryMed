d3a7d95fbfee1c2d3b99f320a9339e49f2e5b1ca
yannrivault <yann.rivault@ehesp.fr>
2018-07-19 13:06:41 +0200

    url debug

--- queryMed/R/test.R
+++ queryMed/R/test.R
@@ -1,5 +1,5 @@
 test <- function(path=""){
-  download.file("http://data.bioontology.org/ontologies/ATC/submissions/9/",path)
+  download.file("http://data.bioontology.org/ontologies/ATC/submissions/9/download",path)
   exData = load.rdf(path, format="TURTLE")
   print(exData)
   
