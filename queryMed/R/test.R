7b14abd153f1651f04a283cae1d5a6cd17461ab9
yannrivault <yann.rivault@ehesp.fr>
2018-07-19 14:38:46 +0200

    test -> get_ontology_from_bioportal

--- queryMed/R/test.R
+++ queryMed/R/test.R
@@ -1,7 +1,7 @@
 require(rrdf)
-test <- function(path="",apikey=""){
-  download.file(paste("http://data.bioontology.org/ontologies/ATC/submissions/9/download?apikey=",apikey,sep=""),path)
-  exData = load.rdf(path, format="TURTLE")
+get_ontology_from_bioportal <- function(ontology, path="", apikey=""){
+  download.file(paste(c("http://data.bioontology.org/ontologies/",toupper(ontology),"/download?apikey=",apikey),collapse=""),paste(path,"ATC",""))
+  exData = load.rdf(paste(path,"ATC",""), format="TURTLE")
   print(class(exData))
   
 }
