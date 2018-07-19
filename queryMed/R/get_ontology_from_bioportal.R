377191f18e00accd0e545a72b69be36fdc84ac0a
yannrivault <yann.rivault@ehesp.fr>
2018-07-19 15:33:58 +0200

    added rdf format

--- queryMed/R/get_ontology_from_bioportal.R
+++ queryMed/R/get_ontology_from_bioportal.R
@@ -3,7 +3,7 @@ get_ontology_from_bioportal <- function(ontology, apikey=""){
   path=file.path(file.path(find.package("queryMed"), c("data"),"ontologies"))
   dir.create(path, showWarnings = FALSE)
   
-  download.file(paste(c("http://data.bioontology.org/ontologies/",toupper(ontology),"/download?apikey=",apikey),collapse=""),paste(path,"ATC",""))
+  download.file(paste(c("http://data.bioontology.org/ontologies/",toupper(ontology),"/download?apikey=",apikey,"&download_format=rdf"),collapse=""),paste(path,"ATC",""))
   ontology = load.rdf(paste(path,"ATC",""), format="TURTLE")
   return(ontology)
   
