97d8f24af0f9f0b7fd98c518e1d31657b8ad0d4d
yannrivault <yann.rivault@ehesp.fr>
2018-08-01 14:05:36 +0200

    small update

--- queryMed/R/get_ontology_from_bioportal.R
+++ queryMed/R/get_ontology_from_bioportal.R
@@ -3,8 +3,8 @@ get_ontology_from_bioportal <- function(ontology, apikey=""){
   path=file.path(file.path(find.package("queryMed"), c("data"),"ontologies"))
   dir.create(path, showWarnings = FALSE)
   
-  download.file(paste(c("http://data.bioontology.org/ontologies/",toupper(ontology),"/download?apikey=",apikey,"&download_format=rdf"),collapse=""),paste(path,"ATC",""))
-  ontology = load.rdf(paste(path,"ATC",""), format="TURTLE")
+  download.file(paste(c("http://data.bioontology.org/ontologies/",toupper(ontology),"/download?apikey=",apikey),collapse=""),paste(path,"ontology",""))
+  ontology = load.rdf(paste(path,"ontology",""), format="TURTLE")
   return(ontology)
   
 }
