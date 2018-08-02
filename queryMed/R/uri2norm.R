f234410b3e5670b6968bbe7475e68379d25b3aeb
yannrivault <yann.rivault@ehesp.fr>
2018-08-02 15:07:30 +0200

    small improvement

--- queryMed/R/uri2norm.R
+++ queryMed/R/uri2norm.R
@@ -7,16 +7,17 @@ uri2norm <- function(X){
     class(X) <- "character"
     uris <- apply(X,FUN=function(x) str_detect(x,"http"), MARGIN=2)
 
-    X[which(uris)] <- gsub(".*/|>", "", X[which(uris)])
+    X[which(uris)] <- gsub(".*[#/]|>", "", X[which(uris)])
     X[which(uris)] <- gsub("(^\\s*|\\s*$)","",X[which(uris)], perl=T)
-    X[which(uris)] <- gsub("[A-z0-9]*:","",X[which(uris)])
+    X[which(uris)] <- gsub("[A-z0-9]*[:]","",X[which(uris)])
     
     if("tbl" %in% class.X){return(as_tibble(X))}
     else{return(as.data.frame(X))}
     }
   else {
-    X <- gsub(".*/|>", "", X)
+    X <- gsub(".*[#/]|>", "", X)
     X <- gsub("(^\\s*|\\s*$)","",X, perl=T)
+    X <- gsub("[A-z0-9]*[:]","",X)
     return(X)
    }
 }
