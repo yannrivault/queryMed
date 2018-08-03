b2382cbe6397dc046660803ef5362c40bb0e0cf6
yannrivault <yann.rivault@ehesp.fr>
2018-08-03 10:51:14 +0200

    upgraded with filter_at from dplyr added warnings changed warnings to errors

--- queryMed/R/find_pairs.R
+++ queryMed/R/find_pairs.R
@@ -6,7 +6,8 @@ find_pairs<- function(items,target,target_elements){
     return("No known relations")
   }
   
-  target2 = target[as.character(target[,target_elements[1]])%in%items & as.character(target[,target_elements[2]])%in%items, ]
+  target2 <- filter_at(target,vars(target_elements), all_vars(. %in% items))
+  
   res = target2[apply(target2[,target_elements], 1, function(x){ length(intersect(items, x)) }) ==2, ]
   
   if(nrow(res)==0){
--- queryMed/R/find_relations.R
+++ queryMed/R/find_relations.R
@@ -1,6 +1,25 @@
 find_relations <- function(data.x,data_indices,data.y=NULL,data_elements.x=NULL,data_elements.y=NULL,target,target_elements,progress="none"){
   
+  options(warn=2)
+  
+  if(!all(target_elements %in% colnames(target))){
+    warning("target_elements must be column name(s) from target")
+  }
+  
   if(!is.null(data.y)){
+    
+    if(!(data_indices %in% colnames(data.x)) & (data_indices %in% colnames(data.y))){
+      warning("data_indices must be a column name from both data.x and data.y")
+    }
+    
+    if(!(data_elements.x %in% colnames(data.x))){
+      warning("data_elements.x must be a column name from data.x")
+    }
+    
+    if(!(data_elements.y %in% colnames(data.y))){
+      warning("data_elements.y must be a column name from data.y")
+    }
+    
     data_vocab = unique(c(as.vector(unlist(t(data.x[,data_elements.x]))),as.vector(unlist(t(data.y[,data_elements.y])))))
     target_vocab = unique(as.vector(unlist(t(target[,target_elements]))))
     vocab = intersect(data_vocab,target_vocab)
@@ -16,6 +35,15 @@ find_relations <- function(data.x,data_indices,data.y=NULL,data_elements.x=NULL,
   }
   
   else{
+    
+    if(!(data_indices %in% colnames(data.x))){
+      warning("data_indices must be a column name from data.x")
+    }
+    
+    if(!(data_elements.x %in% colnames(data.x))){
+      warning("data_elements.x must be a column name from data.x")
+    }
+    
     data_vocab = as.vector(unlist(t(data.x[,data_elements.x])))
     target_vocab = as.vector(unlist(t(target[,target_elements])))
     vocab = intersect(data_vocab,target_vocab)
@@ -26,10 +54,12 @@ find_relations <- function(data.x,data_indices,data.y=NULL,data_elements.x=NULL,
     data <- data[(data[,"elements"]%in%vocab),]
   }
   
-  target <- target[as.character(target[,target_elements[1]])%in%vocab & as.character(target[,target_elements[2]])%in%vocab, ]
+  target <- filter_at(target, vars(target_elements),all_vars(. %in% vocab))
   
   
-  temp=dlply(data, data_indices, function(x) find_pairs(x[,"elements"],target=target,target_elements=target_elements),.progress=progress)
-  return(temp)
+temp=dlply(data, data_indices, function(x) find_pairs(x[,"elements"],target=target,target_elements=target_elements),.progress=progress)
+return(temp)
+  
+options(warn=1)
   
 }
